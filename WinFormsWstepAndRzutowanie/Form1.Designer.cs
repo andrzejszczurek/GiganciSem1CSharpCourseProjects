670f4e4b0a247b2c027faa16f9c3e7cae4a674ef
Andrzej Szczurek <andrzej.szczurek91@gmail.com>
2018-11-01 15:29:42 +0100

    Third lesson impl

---
+++ WinFormsWstepAndRzutowanie/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsWstepAndRzutowanie
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.btnOblicz = new System.Windows.Forms.Button();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label5 = new System.Windows.Forms.Label();
+         this.tbWynik = new System.Windows.Forms.TextBox();
+         this.tbFizyka = new System.Windows.Forms.TextBox();
+         this.tbPolski = new System.Windows.Forms.TextBox();
+         this.tbMatematyka = new System.Windows.Forms.TextBox();
+         this.tbAngielski = new System.Windows.Forms.TextBox();
+         this.tbInformatyka = new System.Windows.Forms.TextBox();
+         this.label6 = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // btnOblicz
+         // 
+         this.btnOblicz.Location = new System.Drawing.Point(15, 183);
+         this.btnOblicz.Name = "btnOblicz";
+         this.btnOblicz.Size = new System.Drawing.Size(171, 48);
+         this.btnOblicz.TabIndex = 0;
+         this.btnOblicz.Text = "Oblicz";
+         this.btnOblicz.UseVisualStyleBackColor = true;
+         this.btnOblicz.Click += new System.EventHandler(this.btnOblicz_Click);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(39, 47);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(38, 13);
+         this.label1.TabIndex = 1;
+         this.label1.Text = "Polski:";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(25, 104);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(52, 13);
+         this.label2.TabIndex = 2;
+         this.label2.Text = "Angielski:";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(9, 18);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(68, 13);
+         this.label3.TabIndex = 3;
+         this.label3.Text = "Matematyka:";
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(12, 132);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(65, 13);
+         this.label4.TabIndex = 4;
+         this.label4.Text = "Informatyka:";
+         // 
+         // label5
+         // 
+         this.label5.AutoSize = true;
+         this.label5.Location = new System.Drawing.Point(37, 76);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(40, 13);
+         this.label5.TabIndex = 5;
+         this.label5.Text = "Fizyka:";
+         // 
+         // tbWynik
+         // 
+         this.tbWynik.Location = new System.Drawing.Point(83, 157);
+         this.tbWynik.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbWynik.Name = "tbWynik";
+         this.tbWynik.Size = new System.Drawing.Size(100, 20);
+         this.tbWynik.TabIndex = 6;
+         // 
+         // tbFizyka
+         // 
+         this.tbFizyka.Location = new System.Drawing.Point(83, 73);
+         this.tbFizyka.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbFizyka.Name = "tbFizyka";
+         this.tbFizyka.Size = new System.Drawing.Size(100, 20);
+         this.tbFizyka.TabIndex = 7;
+         // 
+         // tbPolski
+         // 
+         this.tbPolski.Location = new System.Drawing.Point(83, 44);
+         this.tbPolski.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbPolski.Name = "tbPolski";
+         this.tbPolski.Size = new System.Drawing.Size(100, 20);
+         this.tbPolski.TabIndex = 8;
+         // 
+         // tbMatematyka
+         // 
+         this.tbMatematyka.Location = new System.Drawing.Point(83, 15);
+         this.tbMatematyka.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbMatematyka.Name = "tbMatematyka";
+         this.tbMatematyka.Size = new System.Drawing.Size(100, 20);
+         this.tbMatematyka.TabIndex = 9;
+         // 
+         // tbAngielski
+         // 
+         this.tbAngielski.Location = new System.Drawing.Point(83, 101);
+         this.tbAngielski.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbAngielski.Name = "tbAngielski";
+         this.tbAngielski.Size = new System.Drawing.Size(100, 20);
+         this.tbAngielski.TabIndex = 10;
+         // 
+         // tbInformatyka
+         // 
+         this.tbInformatyka.Location = new System.Drawing.Point(83, 129);
+         this.tbInformatyka.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+         this.tbInformatyka.Name = "tbInformatyka";
+         this.tbInformatyka.Size = new System.Drawing.Size(100, 20);
+         this.tbInformatyka.TabIndex = 11;
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.Location = new System.Drawing.Point(31, 160);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(46, 13);
+         this.label6.TabIndex = 12;
+         this.label6.Text = "WYNIK:";
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(204, 249);
+         this.Controls.Add(this.label6);
+         this.Controls.Add(this.tbInformatyka);
+         this.Controls.Add(this.tbAngielski);
+         this.Controls.Add(this.tbMatematyka);
+         this.Controls.Add(this.tbPolski);
+         this.Controls.Add(this.tbFizyka);
+         this.Controls.Add(this.tbWynik);
+         this.Controls.Add(this.label5);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.btnOblicz);
+         this.Name = "Form1";
+         this.Text = "Średnia ocen";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button btnOblicz;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.TextBox tbWynik;
+      private System.Windows.Forms.TextBox tbFizyka;
+      private System.Windows.Forms.TextBox tbPolski;
+      private System.Windows.Forms.TextBox tbMatematyka;
+      private System.Windows.Forms.TextBox tbAngielski;
+      private System.Windows.Forms.TextBox tbInformatyka;
+      private System.Windows.Forms.Label label6;
+   }
+}
+
---
+++ WinFormsWstepAndRzutowanie/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsWstepAndRzutowanie
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         // na chwilę obecną to co musimy wiedzieć o tej części kodu to, że wszystko co jest w tych klamrach wykonuję się
+         // w momencie tworzenia naszego okienka - jest to miejsce gdzie możemy ustawiać dodatkowe własności kontrolek formularza
+
+         // TUTAJ JESZCZE KONTROLKI NIE ISTNIEJĄ
+         InitializeComponent(); // ta metoda odpowiada za stworzenie wszystkich kontrolek na formularzu
+         // TUTAJ JUŻ KONTROLKI SĄ I MOŻNA SIĘ DO NICH ODWOŁYWAC
+
+         // to co chcemy ustawić to właściwość ReadOnly dla TextBoxa na wynik
+         // wynik nie powinien być edytowalny, ale tylko do odczytu
+
+         tbWynik.ReadOnly = true; // powoduje, że zawartość kontrolki jest tylko do odczytu
+      }
+
+      private void btnOblicz_Click(object sender, EventArgs e)
+      {
+         // wszystlo co napiszemy tutaj wykona się każdorazowo po naciśnięciu przypisku Oblicz
+         // technicznie jest to metoda, która jest podpięta pod zdarzenie kliknięcia przypisku
+         // o metoda i zdarzeniach będziemy się jeszcze uczyć.
+        
+         // pobieramy wartości z TexBoxów i zapisujemy do zmiennych.
+         // wszystko co jest w TexbBoxie jest przechowywane jako tekst
+
+         string txtMatematyka = tbMatematyka.Text; // pobranie zawartości tb i wpisanie do zmiennej
+         string txtPolski = tbPolski.Text;
+         string txtFizyka = tbFizyka.Text;
+         string txtAngielski = tbAngielski.Text;
+         string txtInformatyka = tbInformatyka.Text;
+
+         // teraz parsujemy wartości zapisane tekstowo na liczby (zakładamy, że oceny sa liczbami całkowitymi)
+
+         int mat = int.Parse(txtMatematyka);
+         int pol = int.Parse(txtPolski);
+         int fiz = int.Parse(txtFizyka);
+         int ang = int.Parse(txtAngielski);
+         int inf = int.Parse(txtInformatyka);
+
+         // przygotowujemy się do obliczenia średniej
+         int suma = mat + pol + fiz + ang + inf;
+         int liczbaPrzedmiotow = 6;
+
+         // srednia nie koniecznie będzie liczbą całkowitą. 
+         // dzieleniem w naszym przypadku jest dzieleniem całkowitym, bo wykonujemy je na dwóch liczbach całkowitych
+         // int / int = int, przez co stracilibyśmy część dziesiętną średniej. Zeby temu zapobieć potrzebujemy mieć dzielną jako lczbę zmiennoprzecinkową
+         // w tym wypadku nalezy zastosować rzutowanie - zamianę jednego typu na inny
+         double srednia = (double)suma / 6;  // rzutujemy sume na typ double i dopiero dzielimy
+
+
+         // to tb można wpisać tylko tekst dlatego musimy srednia zamienić na tekst
+         // służy do tego metoda ToString() - każda zmienna, nie zależnie od typu posiada taką metodę
+         string txtSrednia = srednia.ToString();
+
+         tbWynik.Text = txtSrednia; // ustawiamy właściwość Text TextBoxa tbWynik na naszą obliczoną średnią
+
+      }
+   }
+}
